f2c3015041f87fe770c108dcbddab86b695becd7
Sara Rahali <rahalisara@hotmail.fr>
2021-11-23 14:06:20 +0100

    methods stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,21 @@
 class StoriesController < ApplicationController
+
+  def index
+    @stories = Stories.all
+  end
+
+  def show
+    @stories = Stories.find(params[:id])
+  end
+
+  private
+
+  def stories_params
+    params.require(:stories).permit(
+      :title,
+      :description,
+      :photo,
+      :category
+    )
+  end
 end
