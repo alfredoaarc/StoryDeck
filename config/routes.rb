9b7c3d0f7f6014ca93c559119ba94a4837662443
Sara Rahali <rahalisara@hotmail.fr>
2021-11-23 17:07:50 +0100

    stories controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :businesses do
-    resources :stories, only: [:index, :show, :new, :create]
+    resources :stories
   end
   resources :chatrooms do
     resources :messages
   end
-  # resources :campaigns, only: [:index, :show, :new, :create]
 end
