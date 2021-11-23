a9412d63816bc1f3df223566329346eeca33b70e
alfredorico <alfredoaarc11@gmail.com>
2021-11-23 16:04:31 +0100

    controllersalfredo

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,26 @@
 class BusinessesController < ApplicationController
+
+   def index
+    @business = Business.all
+  end
+
+
+  def new
+    @business = Business.new
+  end
+
+  def create
+    @business = Business.new(business_params)
+    @business.user = current_user
+    @business.save
+    redirect_to business_path(@business)
+  end
+
+  def business_params
+  params.require(:business).permit(
+    :name,
+    :description,
+    :website
+  )
+  end
 end
