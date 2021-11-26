1e043f4f8e6d9bbf24ac5ada203dfa3df80f657f
monica randriamialy <mo@novapixels.net>
2021-11-26 14:33:05 +0100

    redirections after sign in ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
   def home
     @skip_navbar = true
     @skip_footer = true
-    # redirect_to root_path if user_signed_in? # this line is creating too many redirections at some point. Why?
+    redirect_to dashboard_path if user_signed_in?
   end
 
   def dashboard
