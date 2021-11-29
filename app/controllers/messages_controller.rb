16f97e267bb2ff23d85b46b00a73cd202abcf246
monica randriamialy <mo@novapixels.net>
2021-11-29 13:37:07 +0100

    Footer updated

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,23 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: %i[index show]
+  def index
+    @messages = Message.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def messages_params
+    params.require(:message).permit(
+      :chatroom_id,
+      :user_id
+    )
+  end
 end
