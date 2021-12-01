e0e7c9768201ddfb322eb500261d1f30be16e1fd
monica randriamialy <mo@novapixels.net>
2021-12-01 11:28:23 +0100

    update after removed js tests with Isa

---
+++ app/javascript/channels/navbar.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  hamburger.addEventListener("click", mobileMenu);
+
+
+  const navLink = document.querySelectorAll(".nav-link");
+
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+
+  export{mobileMenu, closeMenu}
