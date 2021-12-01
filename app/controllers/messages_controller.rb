f75308bff9cb2667304ea3a9a28c0b0037fd671b
alfredorico <alfredoaarc11@gmail.com>
2021-12-01 09:40:41 +0100

    no se bro

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,21 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: %i[index show]
-  def index
-    @messages = Message.all
+   def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      ChatroomChannel.broadcast_to(
+      @chatroom,
+      render_to_string(partial: "message", locals: { message: @message })
+  )
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+
+    else
+      render "chatrooms/show"
+    end
+    private
+    def message_params
+      params.require(:message).permit(:content, :chatroom_id, :user_id)
+    end
   end
-
-  def show
-
-  end
-
-  private
-
-  def set_message
-    @message = Message.find(params[:id])
-  end
-
-  def messages_params
-    params.require(:message).permit(
-      :chatroom_id,
-      :user_id
-    )
-  end
-end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,8 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  import { initChatroomCable } from "../channels/chatroom_channel";
+
   const hamburger = document.querySelector(".hamburger");
   const navMenu = document.querySelector(".nav-menu");
 
